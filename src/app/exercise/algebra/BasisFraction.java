e652c14eecf7e2094a5b2d3303f6ae8721b2c0cc
moritzreinmoeller <moritzreinmoeller@unitybox,de>
2021-05-11 17:20:57 +0200

    main.java

--- src/app/exercise/algebra/BasisFraction.java
+++ src/app/exercise/algebra/BasisFraction.java
@@ -1,6 +1,6 @@
 package app.exercise.algebra;
 
-public abstract class BasisFraction {
+public abstract class BasisFraction implements Fractional{
 
     protected abstract void setND(long numerator, long denominator);
 }
